bc07676c4e265e4ef8f5e772b9b70a48ea9b6503
s1250103 <s1250103@u-aizu.ac.jp>
2020-11-21 15:06:01 +0900

    no message

---
+++ tools4RCH/github.py
@@ -0,0 +1,6 @@
+!git config --global user.name 'Moriya Ryota'
+!git config --global user.email 's1250103@u-aizu.ac.jp'
+!git config --global push.default current
+!git config --global pull.ff only
+
+!git clone https://s1250103:Filry7gp@github.com/s1250103/Public_mori-lab.git
